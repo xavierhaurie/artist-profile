5c304400de099f9ed110859510b6298492c3435d
xavier_haurie <xavier.haurie@gmail.com>
2026-07-30 13:09:00 -0400

    clean up unused classes

--- profile-venue-service/src/main/java/com/artistprofile/dto/ProfileConnectionDTO.java
+++
@@ -1,9 +0,0 @@
-package com.artistprofile.dto;
-
-import com.artistprofile.entity.ProfileVenue;
-
-public record ProfileConnectionDTO(String blurb, ProfileDTO profile) {
-    static ProfileConnectionDTO from(ProfileVenue pv) {
-        return new ProfileConnectionDTO(pv.getBlurb(), ProfileDTO.from(pv.getProfile()));
-    }
-}
--- profile-venue-service/src/main/java/com/artistprofile/dto/VenueConnectionDTO.java
+++
@@ -1,9 +0,0 @@
-package com.artistprofile.dto;
-
-import com.artistprofile.entity.ProfileVenue;
-
-public record VenueConnectionDTO(String blurb, VenueDTO venueDTO) {
-    static VenueConnectionDTO from(ProfileVenue pv) {
-        return new VenueConnectionDTO(pv.getBlurb(), VenueDTO.from(pv.getVenue()));
-    }
-}
