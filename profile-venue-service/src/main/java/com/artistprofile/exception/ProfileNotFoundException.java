7d3625d4cf29b4bc053d5a99696762b7b67c441e
xavier_haurie <xavier.haurie@gmail.com>
2026-07-30 10:20:46 -0400

    move exception classes to their own folder

--- profile-venue-service/src/main/java/com/artistprofile/exception/ProfileNotFoundException.java
+++ profile-venue-service/src/main/java/com/artistprofile/exception/ProfileNotFoundException.java
@@ -1,4 +1,4 @@
-package com.artistprofile.services;
+package com.artistprofile.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
--- profile-venue-service/src/main/java/com/artistprofile/services/VenuesController.java
+++ profile-venue-service/src/main/java/com/artistprofile/services/VenuesController.java
@@ -1,5 +1,6 @@
 package com.artistprofile.services;
 
+import com.artistprofile.exception.VenueNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
